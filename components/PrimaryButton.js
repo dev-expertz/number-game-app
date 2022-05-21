bfb520879d7b49acb02dba20696831ff74629f5c
Dev Expertz <dev.expertz@outlook.com>
2022-05-21 22:38:10 +0500

    Initial Commit

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { Pressable, View, Text } from "react-native";
+
+export default function PrimaryButton(props){
+    return (
+        <Pressable>
+            <View>
+                <Text></Text>
+            </View>
+        </Pressable>
+    )
+}
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+export default function StartGameScreen() {
+  return (
+    <View style={styles.container}>
+      <TextInput />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
